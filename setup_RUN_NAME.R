f1c7c68aed3d1c271598f189d377358508dc0b21
Jason Everett <jaseeverett@gmail.com>
2020-03-17 09:45:23 +1100

    Update with model runs

--- setup_RUN_NAME.R
+++ setup_RUN_NAME.R
@@ -2,18 +2,19 @@
 ## Models multiple zooplankton functional groups, and three fish groups
 ## This code is to run the model across multiple cores
 ##
-## Jason's version for UNSWs Katana
 ##
 ## Updated Friday 31st January 2020
 ## Updated Friday 13th March 2020
+## Updated Tuesday 17th March 2020
 
 source("fZooMSS_Model.R") #source the model code
 
+save_all <- 1 # Should we save all time steps
+HPC <- 0 # Is this being run on a HPC or will we choose the cell
+
 enviro_data <- readRDS("envirofull_20200312.RDS") # Load environmental data
 
-enviro_data$tmaxx <- 1 # Set length of simulation (years)
-
-save_all <- 0
+enviro_data$tmaxx <- 500 # Set length of simulation (years)
 
 #job specifics
 Groups <- read.csv("TestGroups.csv") # Load in functional group information
@@ -23,11 +24,12 @@ Groups <- read.csv("TestGroups.csv") # Load in functional group information
 # FALSE = NO (Use for PPMR=100 and PPMR=1000 runs).
 fixed_filterPPMR <- TRUE
 
-jobname <- 'DATE_JOB.NAME' #job name used on queue
+jobname <- 'DATE_JOBNAME' #job name used on queue: Recommend: YYYYMMDD_AbbrevExperimentName.
 
-ID <- as.integer(Sys.getenv('PBS_ARRAY_INDEX')) # Get the array run number
-
-ID <- 1 # Test Run
+if (HPC == 1){
+  ID <- as.integer(Sys.getenv('PBS_ARRAY_INDEX'))} else { # Get the array run number
+    ID <- 100 # Test Run
+    } 
 
 ID_char <- sprintf("%04d",ID) # Set the ID as a 4 digit character so it will sort properly
 
