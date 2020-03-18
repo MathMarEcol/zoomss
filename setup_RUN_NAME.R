f31eb33eaefc781fb87de141436b6781764e815c
Jason Everett <jaseeverett@gmail.com>
2020-03-18 19:21:49 +1100

    Restructure setup file

--- setup_RUN_NAME.R
+++ setup_RUN_NAME.R
@@ -9,20 +9,20 @@
 library(tidyverse)
 source("fZooMSS_Model.R") #source the model code
 
+enviro_data <- read_rds("envirofull_20200317.RDS") # Load environmental data
+enviro_data$tmax <- 10 # Set length of simulation (years)
+Groups <- read_csv("TestGroups.csv") # Load in functional group information
+jobname <- 'DATE_JOBNAME' #job name used on queue: Recommend: YYYYMMDD_AbbrevExperimentName.
+enviro_row <- 1 # Which row of the environmental data do you want to run if HPC=FALSE
 SaveTimeSteps <- TRUE # Should we save all time steps
 HPC <- 0 # Is this being run on a HPC or will we choose the cell
 
-enviro_data <- read_rds("envirofull_20200317.RDS") # Load environmental data
-enviro_data$tmax <- 10 # Set length of simulation (years)
 
-#job specifics
-Groups <- read_csv("TestGroups.csv") # Load in functional group information
-
-jobname <- 'DATE_JOBNAME' #job name used on queue: Recommend: YYYYMMDD_AbbrevExperimentName.
+### No need to change anything below here.
 
 if (HPC == 1){
   ID <- as.integer(Sys.getenv('PBS_ARRAY_INDEX'))} else { # Get the array run number on HPC
-    ID <- 1 # Test Run
+    ID <- enviro_row
   } 
 ID_char <- sprintf("%04d",ID) # Set the ID as a 4 digit character so it will sort properly
 
