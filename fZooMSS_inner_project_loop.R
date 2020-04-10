e87f4186b3d57a048326d5ef5866302ae8cbb78e
patricksykes <52962150+patricksykes@users.noreply.github.com>
2020-04-11 00:28:16 +1000

    Adjust indices to correct calculations (R counts from 1, C++ counts from 0)

--- fZooMSS_inner_project_loop.R
+++ fZooMSS_inner_project_loop.R
@@ -4,11 +4,7 @@ fZooMSS_inner_project_loop <- Rcpp::cppFunction('NumericMatrix fZooMSS_inner_pro
                 NumericMatrix S, NumericVector w_min_idx, NumericVector w_max_idx) {
 
     for (int i = 0; i < no_sp; i++) {
-<<<<<<< HEAD
         for (int j = w_min_idx[i]; j < w_max_idx[i]-1; j++) {
-=======
-        for (int j = w_min_idx[i]; j < w_max_idx[i]+1; j++) {
->>>>>>> a600138fff77b85bc27384035ed4ecbc9c75ab7b
             niter(i,j) = (Siter(i,j) + Aiter(i,j)*niter(i,j-1)) / Citer(i,j);
             if (j > w_min_idx[i]) {
             n(i,j - 1) <- (S(i,j - 1) + A(i,j - 1) * n(i,j - 2) + B(i,j - 1) * niter(i,j)) / C(i,j - 1);
