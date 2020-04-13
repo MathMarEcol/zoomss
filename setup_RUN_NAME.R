4cd7db36c62d03423731e810cff1f08d0676f3c7
Jason Everett <jaseeverett@gmail.com>
2020-04-13 12:07:36 +1000

    Pass groups to function

--- setup_RUN_NAME.R
+++ setup_RUN_NAME.R
@@ -6,6 +6,7 @@
 ## Updated Friday 13th March 2020
 ## Updated Tuesday 17th March 2020
 ## Updated Tuesday 31st March 2020
+## Updated Monday 13th April 2020
 
 source("fZooMSS_Model.R") #source the model code
 
@@ -22,15 +23,15 @@ SaveTimeSteps <- TRUE # Should we save all time steps
 ### No need to change anything below here.
 if (HPC == TRUE){
   ID <- as.integer(Sys.getenv('PBS_ARRAY_INDEX')) # Get the array run number on HPC
-  } else { 
+  } else {
     ID <- enviro_row
-  } 
+  }
 ID_char <- sprintf("%04d",ID) # Set the ID as a 4 digit character so it will sort properly
 
 input_params <- enviro_data[ID,]
 
 out$model$model_runtime <- system.time(
-  out <- fZooMSS_Model(input_params, SaveTimeSteps)
+  out <- fZooMSS_Model(input_params, Groups, SaveTimeSteps)
 )
 
 saveRDS(out, file = paste0("RawOutput/", jobname, "_", ID_char,".RDS"))
