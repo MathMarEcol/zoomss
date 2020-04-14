336a1a48ac43af9ef31d680acebe7d1b604cc590
Jason Everett <jaseeverett@gmail.com>
2020-04-14 16:30:52 +1000

    Update with further checks

--- setup_RUN_NAME.R
+++ setup_RUN_NAME.R
@@ -11,11 +11,11 @@
 source("fZooMSS_Model.R") #source the model code
 
 enviro_data <- readRDS("envirofull_20200317.RDS") # Load environmental data
-enviro_data$tmax <- 200 # Set length of simulation (years)
+enviro_data$tmax <- 25 # Set length of simulation (years)
 Groups <- read.csv("TestGroups.csv", stringsAsFactors = FALSE) # Load in functional group information
 
 jobname <- 'DATE_JOBNAME' #job name used on queue: Recommend: YYYYMMDD_AbbrevExperimentName.
-enviro_row <- 985 # Which row of the environmental data do you want to run if HPC=FALSE
+enviro_row <- 1 # Which row of the environmental data do you want to run if HPC=FALSE
 
 HPC <- FALSE # Is this being run on a HPC or will we choose the row
 SaveTimeSteps <- TRUE # Should we save all time steps
@@ -35,3 +35,25 @@ out$model$model_runtime <- system.time(
 )
 
 saveRDS(out, file = paste0("RawOutput/", jobname, "_", ID_char,".RDS"))
+
+# source("fZooMSS_CheckIdent.R")
+# out_old <- readRDS("RawOutput/DATE_JOBNAME_0001_20200414.RDS")
+# out_old <- readRDS("RawOutput/DATE_JOBNAME_0350_20200401.RDS")
+# out_old <- readRDS("RawOutput/DATE_JOBNAME_0611_20200401.RDS")
+
+# fZooMSS_CheckIdent(out, out_old)
+
+
+
+# 25 years - after updates
+# user  system elapsed
+# 58.126  11.076  65.322
+# 57.617   8.869  67.118
+
+# 55.924   8.022  63.699 # Removed all list accessing
+# 56.792  10.589  63.379
+
+# 59.514   8.850  68.369 # Added list again
+# 54.737   8.760  63.391 # Remove list accessing again
+# 61.679  11.131  68.390
+# 58.708   9.891  68.621
