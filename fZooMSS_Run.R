f243191251ef72234251a1148f0e8cda8111970c
Jason Everett <jaseeverett@gmail.com>
2020-04-14 16:34:46 +1000

    Major update to remove list queries

--- fZooMSS_Run.R
+++ fZooMSS_Run.R
@@ -7,36 +7,49 @@ fZooMSS_Run <- function(model){
 
   # Pull out some useful parameters - just a shortcut
   param <- model$param
-  dt <- param$dt
-  dx <- param$dx
+  dt <- model$param$dt
+  dx <- model$param$dx
+  ngrps <- model$param$ngrps
+  ngrid <- model$param$ngrid
+  w <- model$param$w
+  W0 <- param$Groups$W0
+  Wmax <- param$Groups$Wmax
+  fish_grps <- model$param$fish_grps
+  assim_eff <- model$assim_eff
+  temp_eff <- model$temp_eff
+  dynam_dietkernel <- model$dynam_dietkernel
+  dynam_growthkernel <- model$dynam_growthkernel
+  dynam_mortkernel <- model$dynam_mortkernel
+  dynam_diffkernel <- model$dynam_diffkernel
+  diff_phyto <- model$diff_phyto
 
-  idx.iter <- 2:model$ngrid
-  idx <- 2:(model$ngrid-1)
-  itimemax  <- param$tmax / param$dt  #max index of time array
+  idx.iter <- 2:ngrid
+  idx <- 2:(ngrid-1)
+  itimemax  <- param$tmax / dt  #max index of time array
 
   if(length(param$zoo_grps) > 1){ # If there's only one zoo group, then you do not need w0idx. All this stuff gives you info about all zoo groups except the smallest zoo group.
-    w0idx <- which(param$Groups$W0 > min(param$Groups$W0) & is.na(param$Groups$Prop) == FALSE)
+    w0idx <- which(W0 > min(W0) & is.na(param$Groups$Prop) == FALSE)
     w0mins <- rep(0, length(w0idx))
     props_z <- param$Groups$Prop[w0idx] # Zooplankton proportions
 
     for(i in 1:length(w0idx)){
       # Which size class is the smallest size class for each functional group
-      w0mins[i] <- which(round(log10(model$w), digits = 2) == param$Groups$W0[w0idx[i]])
+      w0mins[i] <- which(round(log10(w), digits = 2) == W0[w0idx[i]])
     }
   }
 
   # Matrices for MvF and MvF-D numeric solution
-  A.iter <- matrix(0, nrow = model$param$ngrps, ncol = model$ngrid)
-  C.iter <- matrix(0, nrow = model$param$ngrps, ncol = model$ngrid)
-  S.iter <- matrix(0, nrow = model$param$ngrps, ncol = model$ngrid)
+  A.iter <- matrix(0, nrow = ngrps, ncol = ngrid)
+  C.iter <- matrix(0, nrow = ngrps, ncol = ngrid)
+  S.iter <- matrix(0, nrow = ngrps, ncol = ngrid)
 
-  A <- matrix(0, nrow = model$param$ngrps, ncol = model$ngrid)
-  B <- matrix(0, nrow = model$param$ngrps, ncol = model$ngrid)
-  C <- matrix(0, nrow = model$param$ngrps, ncol = model$ngrid)
-  S <- matrix(0, nrow = model$param$ngrps, ncol = model$ngrid) # Previous abundance
+  A <- matrix(0, nrow = ngrps, ncol = ngrid)
+  B <- matrix(0, nrow = ngrps, ncol = ngrid)
+  C <- matrix(0, nrow = ngrps, ncol = ngrid)
+  S <- matrix(0, nrow = ngrps, ncol = ngrid) # Previous abundance
 
   # Temporary Matrices that get updated each time step some of these saved for output
-  N <- matrix(model$N[1,,], nrow = model$param$ngrps, ncol = model$ngrid) # Abundances of functional groups, dim 1 = groups, dim 2 = size classes
+  N <- matrix(model$N[1,,], nrow = ngrps, ncol = ngrid) # Abundances of functional groups, dim 1 = groups, dim 2 = size classes
 
   pb <- txtProgressBar(min = 0, max = itimemax, initial = 1, style = 3) # Initial progress bar
 
@@ -45,29 +58,29 @@ fZooMSS_Run <- function(model){
 
     setTxtProgressBar(pb, itime) # Update progress bar
 
-    growth_multiplier <- colSums(N*model$assim_eff) # 1 x n_sizes
-    predation_multiplier <- N*model$temp_eff # n_species x n_sizes
-    diffusion_multiplier <- colSums(N*(model$assim_eff^2)) # 1 x n_sizes
+    growth_multiplier <- colSums(N * assim_eff) # 1 x n_sizes
+    predation_multiplier <- N * temp_eff # n_species x n_sizes
+    diffusion_multiplier <- colSums(N * (assim_eff^2)) # 1 x n_sizes
 
     ### DO GROWTH
-    sw <- sweep(model$dynam_growthkernel, 3, growth_multiplier, '*') # n_species x n_sizes x n_sizes
+    sw <- sweep(dynam_growthkernel, 3, growth_multiplier, '*') # n_species x n_sizes x n_sizes
     ap <- aperm(sw, c(3,1,2)) # n_sizes x n_species x n_sizes
     cs <- colSums(ap) # n_species x n_sizes
     gg <- model$ingested_phyto + cs
     rm(sw, ap, cs)
 
     ### DO MORTALITY
-    sw2 <- sweep(model$dynam_mortkernel, c(2,3), predation_multiplier, '*') # n_sizes x n_species x n_sizes
+    sw2 <- sweep(dynam_mortkernel, c(2,3), predation_multiplier, '*') # n_sizes x n_species x n_sizes
     ap2 <- aperm(sw2, c(2,3,1))
-    M2 <- colSums(colSums(ap2)) # 1 x n_sizes
+    M2 <- .colSums(colSums(ap2),ngrid,ngrid) # 1 x n_sizes
     Z <- sweep(model$M_sb + model$fish_mort, 2, M2, '+') # Total dynamic spectrum mortality (n_species x n_sizes)
     rm(sw2, ap2)
 
     ### DO DIFFUSION
-    sw3 <- sweep(model$dynam_diffkernel, 3, diffusion_multiplier, '*')
+    sw3 <- sweep(dynam_diffkernel, 3, diffusion_multiplier, '*')
     ap3 <- aperm(sw3, c(3,1,2))
     cs3 <- colSums(ap3)
-    diff <- model$diff_phyto + cs3
+    diff <- diff_phyto + cs3
     rm(sw3, ap3, cs3)
 
     ### MvF WITH DIFFUSION ALGORITHM
@@ -84,12 +97,11 @@ fZooMSS_Run <- function(model){
     S[,idx] <- N[,idx]
 
     ### RFH - I have a C++ implementation for this but it won't help with speed at this point I don't think.
-    for(i in 1:model$param$ngrps){
+    for(i in 1:ngrps){
 
-      ### RFH - Can't these next few rows be done in fZooMSS_Setup and stored as a vector?
       ## Set size range index for current group
-      curr_min_size <- which(round(log10(model$w), digits = 2) == param$Groups$W0[i])
-      curr_max_size <- which(round(log10(model$w), digits = 2) == param$Groups$Wmax[i])
+      curr_min_size <- which(round(log10(w), digits = 2) == W0[i])
+      curr_max_size <- which(round(log10(w), digits = 2) == Wmax[i])
       idx_curr <- (curr_min_size+1):curr_max_size
 
       for(j in idx_curr){ ## Find the abundance at the next size class with standard MvF
@@ -110,7 +122,6 @@ fZooMSS_Run <- function(model){
     #### Keep smallest fish community size class as equal to equivalent zooplankton size class
     ### Keep smallest zooplankton size class abundnace
     ### for each group locked to others in size spectrum
-
     if(length(param$zoo_grps) > 1){ # If you only have one zoo group, it will be locked to phyto spectrum so you do not need to do this
       for(i in 1:length(w0idx)){
         w_min_curr <- w0mins[i]
@@ -119,14 +130,15 @@ fZooMSS_Run <- function(model){
       }
     }
 
-    fish_mins <- unlist(lapply(param$Groups$W0[param$fish_grps],
-                               function(x){which(round(log10(model$w), digits = 2) == x)}))
+    fish_mins <- unlist(lapply(W0[fish_grps],
+                               function(x){which(round(log10(w), digits = 2) == x)}))
 
-    if(length(param$fish_grps) > 1 & length(param$zoo_grps) > 1){
-      N[param$fish_grps,fish_mins] <- (1/length(param$fish_grps))*(colSums(N[-param$fish_grps,fish_mins]))
+    if(length(fish_grps) > 1 & length(param$zoo_grps) > 1){
+      N[fish_grps,fish_mins] <- (1/length(fish_grps))*(colSums(N[-fish_grps,fish_mins]))
     }else{
-      N[param$fish_grps, fish_mins] <- (1/length(param$fish_grps))*sum(N[-param$fish_grps, fish_mins])
+      N[fish_grps, fish_mins] <- (1/length(fish_grps))*sum(N[-fish_grps, fish_mins])
     }
+
 
     # Save results:
     if((itime %% param$isave) == 0){
@@ -138,24 +150,18 @@ fZooMSS_Run <- function(model){
       micro_phyto_diet <- rowSums(model$diet_micro_phyto*N) # Micro-phytoplankton
 
       ## Functional group diet
-      ### Create an ngrps*ngrid*ngrps*ngrid array of abundances, to save time without sweeps
-      # dim1 = pred groups, dim 2 = pred sizes, dim 3 = prey groups, dim 4 = prey sizes
-      N_array <- aperm(replicate(model$ngrid, N), c(3,1,2))
-      N_array <- aperm(replicate(model$param$ngrps, N_array), c(4,1,2,3))
-      dynam_diet <- rowSums(aperm(rowSums(sweep(model$dynam_dietkernel*N_array, c(1,2), N, "*"), dims = 3), c(1,3,2)), dims = 2)
+      ### Create an ngrps*ngrid*ngrps*ngrid array of abundances, to save time without sweeps: dim1 = pred groups, dim 2 = pred sizes, dim 3 = prey groups, dim 4 = prey sizes
+      N_array_temp <- aperm(replicate(ngrid, N), c(3,1,2))
+      N_array <- aperm(replicate(ngrps, N_array_temp), c(4,1,2,3))
+      dynam_diet <- rowSums(aperm(rowSums(sweep(dynam_dietkernel*N_array, c(1,2), N, "*"), dims = 3), c(1,3,2)), dims = 2)
 
       model$diet[isav,,1:3] <- cbind(pico_phyto_diet, nano_phyto_diet, micro_phyto_diet)
       model$diet[isav,,c(4:(dim(param$Groups)[1]+3))] <- dynam_diet
-
       model$N[isav,,] <- N # Save N by taxa and size
-      model$Abundance[isav,] <- rowSums(model$N[isav,,]) ## Save Total Abundance
-      model$Biomass[isav,] <- rowSums(model$N[isav,,]*matrix(model$w, nrow = model$param$ngrps, ncol = model$ngrid, byrow = TRUE)) ## Save biomass
-      model$M2[isav,,] <- matrix(M2, nrow = model$param$ngrps, ncol = length(M2), byrow=TRUE) # Save predation mortality rates
       model$Z[isav,,] <-  Z ## Save mortality
       model$gg[isav,,] <-  gg ## Save growth
     }
   } # End of time loop
-
   return(model)
 
 }
