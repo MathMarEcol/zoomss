516001bdfa062a7a2aaa5f4fa26e3da00e039cfa
Jason Everett <jaseeverett@gmail.com>
2020-05-29 16:15:01 +1000

    Update the extras

--- fZooMSS_Xtras.R
+++ fZooMSS_Xtras.R
@@ -1,3 +1,13 @@
+# This script contains a list of helper functions which can be used to analyse and plot the ZooMSS output
+
+# Summarise the biomass for each gridcell by species
+fZooMSS_GlobalBiomass = function(res,w) {
+  bioms <- matrix(NA, nrow = length(res), ncol = length(res[[1]][,1])) # Preallocate Fish Proportions
+  for(j in 1:length(res)){
+    bioms[j,] = apply(sweep(res[[j]], 2, w, '*'), 1, sum) # Get the reference/control data
+  }
+  return(bioms)
+}
 
 # Function to calculate the mean of the last 50 % of the model
 fZooMSS_AveOutput = function(x){
@@ -5,6 +15,7 @@ fZooMSS_AveOutput = function(x){
   return(ave_x)
 }
 
+# Remove nonsense attributes if we are working for speed and memory efficiency.
 untibble <- function (tibble) {
   data.frame(unclass(tibble), check.names = FALSE, stringsAsFactors = FALSE)
 }  ## escape the nonsense
