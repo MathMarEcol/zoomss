3b46aa95a37bc5be66852727d48879a8f218fd47
Jason Everett <jaseeverett@gmail.com>
2020-05-29 16:45:24 +1000

    Update fZooMSS_Xtras.R

--- fZooMSS_Xtras.R
+++ fZooMSS_Xtras.R
@@ -1,12 +1,9 @@
 # This script contains a list of helper functions which can be used to analyse and plot the ZooMSS output
 
 # Summarise the biomass for each gridcell by species
-fZooMSS_GlobalBiomass = function(res,w) {
-  bioms <- matrix(NA, nrow = length(res), ncol = length(res[[1]][,1])) # Preallocate Fish Proportions
-  for(j in 1:length(res)){
-    bioms[j,] = apply(sweep(res[[j]], 2, w, '*'), 1, sum) # Get the reference/control data
-  }
-  return(bioms)
+fZooMSS_SpeciesBiomass = function(res,w) {
+  Biomass <- map(res, function(x) apply(sweep(x, 2, w, '*'), 1, sum))
+  return(Biomass)
 }
 
 # Function to calculate the mean of the last 50 % of the model
