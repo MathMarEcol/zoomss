72e5cf4ac65910738dbcbf16ffc68c95a7667c1a
Jason Everett <jaseeverett@gmail.com>
2021-01-13 13:48:14 +1100

    New plotting routines

---
+++ ZooMSS_Plot_GrowthTimeSeries.R
@@ -0,0 +1,27 @@
+# Plot growth by time
+
+ZooMSS_Plot_GrowthTimeSeries <- function(dat){
+
+  gr <- rowSums(dat$model$gg, dims = 2) / length(dat$model$param$w)
+  colnames(gr) <- dat$model$param$Groups$Species
+  gr <- as_tibble(gr)
+  gr$Time <- seq(dat$model$param$dt * dat$model$param$isave,
+                 dat$model$param$tmax,
+                 dat$model$param$dt * dat$model$param$isave)
+  gr <- gr %>%
+    pivot_longer(-Time, names_to = "Species", values_to = "Growth") %>%
+    filter(Growth > 0) %>%
+    mutate(Species = factor(Species, levels = dat$model$param$Groups$Species))
+
+  gg <- ggplot(data = gg, mapping = aes(x = Time, y = log10(Growth), colour = Species)) +
+    geom_line(size = 1) +
+    geom_point(size = 1.2) +
+    scale_color_manual(values = dat$model$param$Groups$PlotColour) +
+    theme_bw() +
+    scale_x_continuous(expand = c(0, 0)) +
+    scale_y_continuous(expand = c(0, 0)) +
+    labs(subtitle = "Growth Rate") +
+    xlab("Time (Years)")
+
+  return(gg)
+}
---
+++ ZooMSS_Plot_PredTimeSeries.R
@@ -0,0 +1,26 @@
+# Plot predation by time
+ZooMSS_Plot_OredTimeSeries <- function(dat){
+
+  Z <- rowSums(dat$model$Z,dims = 2) / length(dat$model$param$w)
+  colnames(Z) <- dat$model$param$Groups$Species
+  Z <- as_tibble(Z)
+  Z$Time <- seq(dat$model$param$dt * dat$model$param$isave,
+                dat$model$param$tmax,
+                dat$model$param$dt * dat$model$param$isave)
+  Z <- Z %>%
+    pivot_longer(-Time, names_to = "Species", values_to = "Mortality") %>%
+    filter(Mortality > 0) %>%
+    mutate(Species = factor(Species, levels = dat$model$param$Groups$Species))
+
+  gg <- ggplot(data = Z, mapping = aes(x = Time, y = Mortality, colour = Species)) +
+    geom_line(size = 1) +
+    geom_point(size = 1.2) +
+    scale_color_manual(values = dat$model$param$Groups$PlotColour) +
+    theme_bw() +
+    scale_x_continuous(expand = c(0, 0)) +
+    scale_y_continuous(expand = c(0, 0)) +
+    labs(subtitle = "Mortality Rate") +
+    xlab("Time (Years)")
+
+  return(gg)
+}
